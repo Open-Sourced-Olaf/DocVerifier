f8998dd7870148c37926c61dbc1979f2fcbf1a7e
anjalisoni3655 <anjalisony32@gmail.com>
2021-04-01 22:10:10 +0530

    pass tab-url to backend

--- flask-api/app.py
+++ flask-api/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request, render_template
 
+
 app = Flask(__name__)  # create an app instance
 
 
--- get-current-tab-url/background.js
+++ get-current-tab-url/background.js
@@ -6,8 +6,20 @@ function myFunction() {
   chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
     console.log("url", tabs[0].url);
     url = tabs[0].url;
-    alert(tabs[0].url);
+
+    document.getElementById("demo").innerHTML = url;
+    fetch("https://localhost:5000/test", {
+      method: "post",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ tabUrl: url }),
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res));
   });
 
-  document.getElementById("demo").innerHTML = url + "anjali";
+  /*
+   */
 }
